654b5ca25fd1e33d08c64564b3495c6acce0460d
pontuswallquist <pontuswallquist@gmail.com>
2022-11-03 11:10:54 +0100

    Fixed recursion and clearer printstatements

--- crypt.py
+++ crypt.py
@@ -80,23 +80,36 @@ class Crypt:
         self.rollDices()
         self.collectCards()
 
+    def printRoundInfo(self, playerNr):
+        print(self.players[playerNr].color, 'turn!')
+        print('Servants available:', self.players[playerNr].servants)
+        print('')
+
     def collectCards(self):
         for place in range(1, 4):
             if self.board[place]['servants']:
-                if str(self.board[place]['servants'][0]).startswith('Red'):
+                if str(self.board[place]['servants'][0]).startswith('Red'): # Maybe change this line
                     self.collectTreasure(0, place)
                 else:
                     self.collectTreasure(1, place)
 
     def rollDices(self):
+        print('')
         for place in range(1,4):
             for servant in self.board[place]['servants']:
-                if str(servant).startswith('Red'):
-                    if servant.roll() >= servant.effort_value:
+                roll = servant.roll()
+                if servant.color == 'Red':
+                    if roll >= servant.effort_value:
                         self.players[0].recoverSingleServant()
+                        print('Red player rolls', roll, 'and recovers a servant')
+                    else:
+                        print('Red player rolls', roll, 'and servant is exhausted')
                 else:
-                    if servant.roll() >= servant.effort_value:
+                    if roll >= servant.effort_value:
                         self.players[1].recoverSingleServant()
+                        print('Blue player rolls', roll, 'and recovers a servant')
+                    else:
+                        print('Blue player rolls', roll, 'and servant is exhausted')
         
 
     def collectTreasure(self, playerNr, place):
@@ -110,78 +123,74 @@ class Crypt:
             self.noServants(playerNr)
 
     def hasServants(self, playerNr):
-        print(self.players[playerNr].color, 'turn!')
+        self.printRoundInfo(playerNr)
         print('1: Claim a card')
         print('2: Recover all servants')
         print('3: Use Treasures')
-        print('')
-        action = input('Choose an action: ')
+        action = self.get_input(1,3)
         if action == '1':
             self.claimCard(playerNr)
             if self.players[playerNr].servants:
                 self.claimAgain(playerNr)
+            if self.players[playerNr].servants:
+                self.claimAgain(playerNr)
+            return
         elif action == '2':
             self.players[playerNr].recoverServants()
-            #end turn
+            return
         elif action == '3':
-            #end turn
-            pass
-        else:
-            print('Invalid input')
-            self.hasServants(playerNr)
+            return
 
-        ####### Check if player has servants left #######
+
     def lastAction(self, playerNr):
-        print(self.players[playerNr].color, 'turn!')
+        self.printRoundInfo(playerNr)
         print('1: Claim a card')
         print('2: Use Treasures')
-        print('-: End turn')
-        print('')
-        action = input('Choose an action: ')
+        print('3: End turn')
+        action = self.get_input(1,3)
         if action == '1':
-            self.claimCard(playerNr)
+            if self.players[playerNr].servants:
+                self.claimCard(playerNr)
         elif action == '2':
-            #end turn
             pass
-        elif action == '-':
-            #pass turn
+        elif action == '3':
             pass
-        else:
-            print('Invalid input')
-            self.lastAction(playerNr)
+        print('===============================================')
+        return
+        
 
     def noServants(self, playerNr):
-        print(self.players[playerNr].color, 'turn')
+        self.printRoundInfo(playerNr)
         print('1: Recover all servants')
         print('2: Use Treasures')
-        action = input('Choose an action: ')
+        action = self.get_input(1,2)
         if action == '1':
             self.players[playerNr].recoverServants()
-            #end turn
         elif action == '2':
-            #end turn
-            pass
-        else:
-            print('Invalid input')
-            self.noServants(playerNr)
+            return
 
     def claimAgain(self, playerNr):
-        print('')
-        action = input('1: Claim again   2: End turn -> ')
-        print('')
+        self.printBoard()
+        self.printRoundInfo(playerNr)
+        action = print('1: Claim again   2: End turn')
+
+        action = self.get_input(1,2)
         if action == '1':
             self.claimCard(playerNr)
         elif action == '2':
-            pass
-        else:
-            print('Invalid input')
-            self.claimAgain(playerNr)
+            return
 
-        ################# fixa det här
-        if self.players[playerNr].servants:
-            self.claimAgain(playerNr)
-        else:
-            pass
+
+    def get_input(self, start, end):
+        while True:
+            try:
+                choice = int(input('\nChoose an action -> '))
+                if choice < start or choice > end:
+                    raise ValueError
+                break
+            except ValueError:
+                print('Invalid input')
+        return str(choice)
 
 
     def get_place_servants_value(self, playerNr):
@@ -190,59 +199,62 @@ class Crypt:
             try:
                 place = int(input('Choose a place: '))
                 if place < 1 or place > 3:
-                    raise ValueError('Invalid place')
+                    raise ValueError
                 break
-            except ValueError as e:
-                print(e)
+            except ValueError:
+                print('Invalid place')
         
         #Check if player has enough servants to use
         while True:
             try:
                 servants = int(input('Choose a number of servants: '))
                 if servants > len(self.players[playerNr].servants):
-                    raise ValueError('You dont have servants for that')
+                    raise ValueError
                 break
-            except ValueError as e:
-                print(e)
+            except ValueError:
+                print('You don\'t have servants for that')
 
         # Check so value is between 1 and 6
         while True:
             try:
                 value = int(input('Choose a value: '))
                 if value < 1 or value > 6:
-                    raise ValueError('Value must be between 1 and 6')
+                    raise ValueError
                 break
-            except ValueError as e:
-                print(e)
+            except ValueError:
+                print('Value must be between 1 and 6')
 
         return place, servants, value
             
 
     def claimCard(self, playerNr):
        
-        place, servants, value = self.get_place_servants_value(playerNr)
+       while True:
+            try:
+                # Get input from player
+                place, servants, value = self.get_place_servants_value(playerNr)
 
-        #Check if card servants is not empty
-        if self.board[place]['servants']:
-            current_bid = 0
-            for dice in self.board[place]['servants']:
-                current_bid += dice.value
-            #Check if player can outbid current bid
-            if current_bid > value*servants:
-                print('You need to bid higher')
-                self.claimCard(playerNr)
-            else:
-                # Push away current servants
-                otherPlayerNr = 0 if playerNr == 1 else 1
-                for i in range(len(self.board[place]['servants'])):
-                    self.board[place]['servants'].pop()
-                    self.players[otherPlayerNr].recoverSingleServant()
+                #Check if card servants is not empty
+                if self.board[place]['servants']:
+                    current_bid = 0
+                    for dice in self.board[place]['servants']:
+                        current_bid += dice.value
+                    #Check if player can outbid current bid
+                    if current_bid >= value*servants:
+                        raise ValueError
+                    # Push away current servants
+                    otherPlayerNr = 0 if playerNr == 1 else 1
+                    for i in range(len(self.board[place]['servants'])):
+                        self.board[place]['servants'].pop()
+                        self.players[otherPlayerNr].recoverSingleServant()
 
-                self.addServant2Card(playerNr, place, servants, value)
-        else:
-            self.addServant2Card(playerNr, place, servants, value)
-        
-        self.printBoard()
+                    self.addServant2Card(playerNr, place, servants, value)
+                    break
+                else:
+                    self.addServant2Card(playerNr, place, servants, value)
+                    break
+            except ValueError:
+                print('You need to bid higher, try again')
 
     def addServant2Card(self, playerNr, place, servants, value):
         for each in range(servants):
@@ -267,8 +279,6 @@ while loop:
     crypt.revealphase()
     crypt.claimphase()
     crypt.collectphase()
-    print(crypt.players[0].collection, crypt.players[0].servants)
-    print(crypt.players[1].collection, crypt.players[1].servants)
     loop = crypt.passTorchphase(loop)
 
 
