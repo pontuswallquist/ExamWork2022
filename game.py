01b90e6faefc945ca6aee0450322e7497c01eaea
pontuswallquist <pontuswallquist@gmail.com>
2022-11-07 08:36:09 +0100

    Next stage testing

--- game.py
+++ game.py
@@ -1,10 +1,2 @@
 import crypt
 
-gamestate = crypt.Crypt()
-game_over = False
-while not game_over:
-    gamestate.revealphase()
-    gamestate.claimphase()
-    gamestate.collectphase()
-    game_over = gamestate.passTorchphase(game_over)
-
