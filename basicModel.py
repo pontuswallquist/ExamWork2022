7eb72d2d2055c6adaac11711b0df30e87d7c6962
pontuswallquist <pontuswallquist@gmail.com>
2022-11-10 09:32:07 +0100

    basic model test

--- basicModel.py
+++ basicModel.py
@@ -1,48 +1,23 @@
 import gym
-import random
-import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.optimizers import Adam
-from rl.agents import DQNAgent
-from rl.policy import BoltzmannQPolicy
-from rl.memory import SequentialMemory
 
-env = gym.make('new game')
-print(env)
-states = env.observation_space.shape[0]
-actions = env.action_space.n
+from stable_baselines3 import DQN
 
-# Deep Q-learning Network model
-def build_model(states, actions):
-    model = Sequential()
-    model.add(Flatten(input_shape=(1, states)))
-    model.add(Dense(24, activation='relu'))
-    model.add(Dense(24, activation='relu'))
-    model.add(Dense(actions, activation='linear'))
-    return model
+env = gym.make("CartPole-v0")
 
-# Deep Q-learning Agent
-def build_agent(model, actions):
-    policy = BoltzmannQPolicy()
-    memory = SequentialMemory(limit=50000, window_length=1)
-    dqn = DQNAgent(model=model, memory=memory, policy=policy,
-                  nb_actions=actions, nb_steps_warmup=10, target_model_update=1e-2)
-    return dqn
+model = DQN("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=10000, log_interval=4)
+model.save("dqn_cartpole")
+print("Model saved")
 
-# Build model and print summary
-model = build_model(states, actions)
-model.summary()
+del model # remove to demonstrate saving and loading
 
-# Build agent and compile
-dqn = build_agent(model, actions)
-dqn.compile(Adam(learning_rate=1e-3), metrics=['mae'])
+model = DQN.load("dqn_cartpole")
 
-# Train agent
-dqn.fit(env, nb_steps=50000, visualize=False, verbose=1)
 
-#_ = dqn.test(env, nb_episodes=15, visualize=True)
-
-# Test agent
-scores = dqn.test(env, nb_episodes=10, visualize=False)
-print(np.mean(scores.history['episode_reward']))
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    if done:
+      obs = env.reset()
