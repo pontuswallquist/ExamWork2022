d0ad55153e138c81df6667678f49dd6773841e7c
pontuswallquist <pontuswallquist@gmail.com>
2022-11-16 17:16:32 +0100

    Trained first model

--- trainAgent.py
+++ trainAgent.py
@@ -5,17 +5,6 @@ from rich.console import Console
 import time
 console = Console()
 
-def get_score_and_winner(state):
-    if state.players[0].score > state.players[1].score:
-        winner = 'Model'
-    elif state.players[0].score < state.players[1].score:
-        winner = 'Random'
-    else:
-        winner = 'Tie'
-
-    return f"Model: {state.players[0].score} Random: {state.players[1].score} Winner: {winner}"
-
-
 
 train = True
 nr_of_games = 100
@@ -33,4 +22,4 @@ end = time.time()
 
 console.print(f"Time: {int((end-start)/3600)} hours and {int(((end-start)%3600)/60)} minutes")
 
-agent.save_model('model_1.h5')
+agent.save_model('model_2.h5')
